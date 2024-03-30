52e3795d3d045f2a9c39ad23e1872be33d0b4c82
pbhalesain@gmail.com <pbhalesain@gmail.com>
2024-03-30 06:38:35 +0000

    hero modification

--- src/components/web/hero.tsx
+++ src/components/web/hero.tsx
@@ -12,36 +12,42 @@ export default function Hero() {
   return (
 
     <div p-2>
-     <div className={styles.heroContainer}>
-      <div className={styles.gradientOverlay}></div>
-      <div className={styles.cloudContainer}>
-        <div className={styles.pinkCloud}></div>
-        <div className={styles.blueCloud}></div>
+      <div className={styles.heroContainer}>
+        <div className={styles.gradientOverlay}></div>
+        <div className={styles.cloudContainer}>
+          <div className={styles.pinkCloud}></div>
+          <div className={styles.blueCloud}></div>
+        </div>
+        <div className={styles.heroContent}>
+          <PageHeader>
+            <Announcement />
+            <PageHeaderHeading>Block Template Boilerplate</PageHeaderHeading>
+            <PageHeaderDescription>
+              Beautifully designed components from @shadcn. Accessible. Customizable. Open Source.
+            </PageHeaderDescription>
+            <PageActions>
+              <Link
+                href="/docs"
+                className={cn(
+                  buttonVariants({ size: "lg", variant: "primary" }),
+                  "font-semibold text-white bg-primary hover:bg-primary-600 rounded-[4rem] transition-colors duration-200"
+                )}
+              >
+                Get Started
+              </Link>
+              <Link
+                target="_blank"
+                rel="noreferrer"
+                href={siteConfig.links.github}
+                className={cn(buttonVariants({ size: "lg", variant: "outline" }), " hover:bg-secondary-600 transition-colors duration-200 rounded-[4rem]")}
+              >
+                <Icons.gitHub className="mr-2 h-4 w-4" />
+                GitHub
+              </Link>
+            </PageActions>
+          </PageHeader>
+        </div>
       </div>
-      <div className={styles.heroContent}>
-      <PageHeader>
-        <Announcement />
-        <PageHeaderHeading>Block Template Boilerplate</PageHeaderHeading>
-        <PageHeaderDescription>
-          Beautifully designed components from @shadcn. Accessible. Customizable. Open Source.
-        </PageHeaderDescription>
-        <PageActions>
-          <Link href="/docs" className={cn(buttonVariants())}>
-            Get Started
-          </Link>
-          <Link
-            target="_blank"
-            rel="noreferrer"
-            href={siteConfig.links.github}
-            className={cn(buttonVariants({ variant: "outline" }))}
-          >
-            <Icons.gitHub className="mr-2 h-4 w-4" />
-            GitHub
-          </Link>
-        </PageActions>
-      </PageHeader>
-      </div>
-    </div>
     </div>
 
   );
